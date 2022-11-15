670d670d1fde6f10d615c40f2098f380fad66462
ShinY Kim <shinykim215@gmail.com>
2022-11-15 13:06:22 -0500

    10.13

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,23 +1,11 @@
 from rest_framework import serializers
 from .models import Category
 
-class CategorySerializer(serializers.Serializer):
+class CategorySerializer(serializers.ModelSerializer):
 
-    pk = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(
-        required=True,
-        max_length=50,
-    )
-    kind = serializers.ChoiceField(
-        choices=Category.CategoryKindChoices.choices,
-    )
-    created_at = serializers.DateTimeField(read_only=True)
-
-    def create(self, validated_data):
-        return Category.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.kind = validated_data.get("kind", instance.kind)
-        instance.save()
-        return instance
+    class Meta:
+        model = Category
+        fields = "__all__"
+        # fields = ("name","kind")
+        # fields = "__all__"
+        # exclude =("create_at")
