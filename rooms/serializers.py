3a680c327ce4254a3807f2060e7b09dc9b2b125c
ShinY Kim <shinykim215@gmail.com>
2022-12-29 14:18:36 -0500

    23.0

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -32,15 +32,20 @@ class RoomDetailSerializer(serializers.ModelSerializer):
         return room.rating()
 
     def get_is_owner(self, room):
-        request = self.context["request"]
-        return room.owner == request.user
+        request = self.context.get("request")
+        if request:
+                return room.owner == request.user
+        return False
 
     def get_is_liked(self, room):
-        request = self.context["request"]
-        return Wishlist.objects.filter(
-            user=request.user,
-            rooms__pk=room.pk,
-        ).exists()
+        request = self.context.get("request")
+        if request:
+            if request.user.is_authenticated:
+                return Wishlist.objects.filter(
+                    user=request.user,
+                    rooms__pk=room.pk,
+                ).exists()
+        return False
 
 class RoomListSerializer(serializers.ModelSerializer):
     rating = serializers.SerializerMethodField()
@@ -55,6 +60,7 @@ class RoomListSerializer(serializers.ModelSerializer):
             "city",
             "price",
             "rating",
+            "is_owner",
             "photos",
         )
 
