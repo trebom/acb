61ad8f2c767efb5fcc0aea6424dc86ecfe18c227
ShinY Kim <shinykim215@gmail.com>
2022-12-31 17:41:26 -0500

    allowed_host

--- config/settings.py
+++ config/settings.py
@@ -196,8 +196,8 @@ if DEBUG:
     CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
     CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
 else:
-    CORS_ALLOWED_ORIGINS = ["https://acf.onrender.com"]
-    CSRF_TRUSTED_ORIGINS = ["https://acf.onrender.com"]
+    CORS_ALLOWED_ORIGINS = ["https://ksflash.xyz"]
+    CSRF_TRUSTED_ORIGINS = ["https://ksflash.xyz"]
 
 CORS_ALLOW_CREDENTIALS = True
 
