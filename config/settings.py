e2b3394dd1ce6fd5f18a323a2bfe6615d7ed3fe0
ShinY Kim <shinykim215@gmail.com>
2022-12-30 20:29:09 -0500

    CORS_ALLOWED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -191,11 +191,15 @@ REST_FRAMEWORK = {
     ]
 } 
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
+if DEBUG:
+    CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
+    CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
+else:
+    CORS_ALLOWED_ORIGINS = ["https://acb.onrender.com"]
+    CSRF_TRUSTED_ORIGINS = ["https://acb.onrender.com"]
 
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
 
 GH_SECRET = env("GH_SECRET")
 
